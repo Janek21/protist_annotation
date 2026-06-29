63d01e186acb0f9a4f54f8d4c6ea42c88f9d4ba7
Janek21 <jan.izquierdo@alum.esci.upf.edu>
2026-06-29 12:14:21 +0200

    clear folder

--- scripts/tables_setup.sh
+++ scripts/tables_setup.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
+#clear folder
+rm protist_table/*html
+rm protist_table/*tsv
+rm protist_table/*txt
+
 #Protist tables
-python protist_table/protist_survey.py --email luejbpvzbzmvbqwnfb@gonrr.net --api-key 25dd740e73e5a092d87db2f6e230e39eae09 --html -o protist_table/strain_protists.tsv --no-assemblies-only
-python protist_table/protist_survey.py --email luejbpvzbzmvbqwnfb@gonrr.net --api-key 25dd740e73e5a092d87db2f6e230e39eae09 --html -o protist_table/protists.tsv
+python protist_table/protist_survey.py --email luejbpvzbzmvbqwnfb@gonrr.net --api-key $NCBI_API_KEY --html -o protist_table/strain_protists.tsv --no-assemblies-only
+echo "Extracted all strains with RNA-seq"
+python protist_table/protist_survey.py --email luejbpvzbzmvbqwnfb@gonrr.net --api-key $NCBI_API_KEY --html -o protist_table/protists.tsv
+echo "Extracted all strains with RNA-seq and an assembly"
 
 #Extraction
 grep "long-read" protist_table/protists.tsv|cut -f4 | sort -u|grep -v taxid > data/protists_taxid.txt
